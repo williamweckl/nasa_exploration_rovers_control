f7f080d880e67c6fcf00ba8a67d451c102bfc7bf
William Weckl <william.weckl@gmail.com>
2021-05-06 21:43:01 -0300

    Initial commit.

---
+++ lib/nasa_exploration_rovers_control.ex
@@ -0,0 +1,19 @@
+defmodule NasaExplorationRoversControl do
+  @moduledoc """
+  Documentation for `NasaExplorationRoversControl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NasaExplorationRoversControl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+end
---
+++ test/nasa_exploration_rovers_control_test.exs
@@ -0,0 +1,8 @@
+defmodule NasaExplorationRoversControlTest do
+  use ExUnit.Case
+  doctest NasaExplorationRoversControl
+
+  test "greets the world" do
+    assert NasaExplorationRoversControl.hello() == :world
+  end
+end
